f5a9ad6c966e73d54a91ea8eee8c3eceed4ee99a
Aaron J. Scantlin <a.scantlin@gmail.com>
2019-02-06 12:46:45 -0600

    Update default-host-firewall.sh

--- default-host-firewall.sh
+++ default-host-firewall.sh
@@ -1,7 +1,7 @@
 # You can use this as a basic starting point for a host-based firewall
 # Run the command directly below this line, AND ONLY THAT COMMAND, on your EC2 instance to download and execute this firewall script
 
-# wget --no-cache https://raw.githubusercontent.com/coffeebro/IT2910-Scripts/master/default-host-firewall.sh && chmod +x default-host-firewall.sh && ./default-host-firewall.sh
+# wget --no-cache https://raw.githubusercontent.com/coffeebro/IT2910-Scripts/master/default-host-firewall.sh && sudo chmod +x default-host-firewall.sh && ./default-host-firewall.sh
 
 # DO NOT MANUALLY ENTER ANY OF THE INFORMATION BELOW THIS COMMENT!
 
